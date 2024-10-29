0f49d3d726e8a18e07935c81144c3de4df8d6e2d
Bruno Peres <brunoperees@gmail.com>
2024-10-28 21:46:04 -0300

    arquivos novos

---
+++ automacao.py
@@ -0,0 +1,17 @@
+import pyautogui
+'''from mouseinfo import mouseInfo
+mouseInfo()
+359,747
+877, 153
+518, 601
+
+'''
+pyautogui.moveTo(375,747,duration=2)
+pyautogui.click()
+pyautogui.moveTo(877,153,duration=2)
+pyautogui.click()
+pyautogui.moveTo(518,601,duration=2)
+pyautogui.click()
+
+pyautogui.dragTo(518,601,duration=2)
+#pyautogui.click(x=1066,y=226,duration=2, clicks=10,button='left')
