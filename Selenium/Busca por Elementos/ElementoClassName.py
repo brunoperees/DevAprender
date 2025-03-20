bfd2dbb3b9719db5d9e11c0ff93523b8222af39c
Bruno Peres <brunoperees@gmail.com>
2025-03-20 13:55:57 -0300

    Atualizado

---
+++ Selenium/Busca por Elementos/ElementoClassName.py
@@ -0,0 +1,39 @@
+from tkinter import Radiobutton
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+
+
+def iniciar_driver():
+    chrome_options = Options()
+    arguments = ['--lang=pt-BR', '--window-size=800,600', '--incognito']
+    for argument in arguments:
+        chrome_options.add_argument(argument)
+
+    chrome_options.add_experimental_option('prefs', {
+        'download.prompt_for_download': False,
+        'profile.default_content_setting_values.notifications': 2,
+        'profile.default_content_setting_values.automatic_downloads': 1,
+
+    })
+    driver = webdriver.Chrome(service=ChromeService(
+        ChromeDriverManager().install()), options=chrome_options)
+
+    return driver
+
+
+driver = iniciar_driver()
+driver.get('https://cursoautomacao.netlify.app/')
+
+logo = driver.find_element(By.CLASS_NAME, 'navbar-brand')
+links_menu = driver.find_elements(By.CLASS_NAME, 'nav-link')
+
+if logo is not None:
+    print('Encontrado a logo')
+if links_menu is not None:
+    print('Encontrados os links do menu')
+
+input('')
+driver.close()
